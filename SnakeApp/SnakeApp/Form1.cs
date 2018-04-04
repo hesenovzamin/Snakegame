e1c720d9b614b82e0ad8108b92e822a76127a8de
hesenovzamin <hesenovzamin@gmail.com>
2018-04-05 00:26:33 +0400

    meals

--- SnakeApp/SnakeApp/Form1.cs
+++ SnakeApp/SnakeApp/Form1.cs
@@ -16,12 +16,14 @@ namespace SnakeApp
         public Button Snake;
         public Button Meal;
         public int Left = 0;
-        public int Top = 30;
+        public int Top = 40;
         public Form1()
         {
             InitializeComponent();
+            this.Height = 10;
+            this.Width = 40;
             Meals();
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 6; i++)
             {
                 Snake = new Button();
                 Snake.Width = 40;
@@ -59,11 +61,37 @@ namespace SnakeApp
                         item.Left += 40;
                     }
                 }
+                if (SnakeLeght[SnakeLeght.Count - 1].Left > this.Width)
+                {
+                    int AB;
+                    int AS = this.Width % 40;
+                    AB = this.Width - AS;
+                    SnakeLeght[SnakeLeght.Count - 1].Width = AB - 80;
+                }
                 if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
                 {
-                    Snake.BackColor = Color.Red;
+                    Snake = new Button();
+                    Snake.Width = 40;
+                    Snake.Height = 40;
+                    Snake.BackColor = Color.Green;
+                    Snake.Left = SnakeLeght[SnakeLeght.Count-1].Left+40;
+                    Snake.FlatStyle = FlatStyle.Flat;
+                    Snake.Top = SnakeLeght[SnakeLeght.Count - 1].Top;
+                    this.Snake.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
+                    Controls.Add(Snake);
+                    SnakeLeght.Add(Snake);
                     Meal.Hide();
+                    Meals();
                 }
+                for (int i = 0; i < SnakeLeght.Count-1; i++)
+                {
+                    if (SnakeLeght[SnakeLeght.Count - 1].Top ==SnakeLeght[i].Top && SnakeLeght[SnakeLeght.Count - 1].Left == SnakeLeght[i].Left)
+                    {
+                        MessageBox.Show("Lose");
+                        this.Hide();
+                    }
+                }
+
             }
             if (e.KeyCode == Keys.Left)
             {
@@ -83,8 +111,26 @@ namespace SnakeApp
                 }
                 if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
                 {
-                    Snake.BackColor = Color.Red;
+                    Snake = new Button();
+                    Snake.Width = 40;
+                    Snake.Height = 40;
+                    Snake.BackColor = Color.Green;
+                    Snake.Left = SnakeLeght[SnakeLeght.Count - 1].Left - 40;
+                    Snake.FlatStyle = FlatStyle.Flat;
+                    Snake.Top = SnakeLeght[SnakeLeght.Count - 1].Top;
+                    this.Snake.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
+                    Controls.Add(Snake);
+                    SnakeLeght.Add(Snake);
                     Meal.Hide();
+                    Meals();
+                }
+                for (int i = 0; i < SnakeLeght.Count-1; i++)
+                {
+                    if (SnakeLeght[SnakeLeght.Count - 1].Top == SnakeLeght[i].Top && SnakeLeght[SnakeLeght.Count - 1].Left == SnakeLeght[i].Left)
+                    {
+                        MessageBox.Show("Lose");
+                        this.Hide();
+                    }
                 }
             }
             if (e.KeyCode == Keys.Up)
@@ -103,10 +149,35 @@ namespace SnakeApp
                         item.Top -= 40;
                     }
                 }
+                if (SnakeLeght[SnakeLeght.Count-1].Top < 0)
+                {
+                    int AB;
+                    int AS = this.Height%40;
+                    AB = this.Height - AS;
+                    SnakeLeght[SnakeLeght.Count - 1].Top = AB-80;
+                }
                 if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
                 {
-                    Snake.BackColor = Color.Red;
+                    Snake = new Button();
+                    Snake.Width = 40;
+                    Snake.Height = 40;
+                    Snake.BackColor = Color.Green;
+                    Snake.Left = SnakeLeght[SnakeLeght.Count - 1].Left;
+                    Snake.FlatStyle = FlatStyle.Flat;
+                    Snake.Top = SnakeLeght[SnakeLeght.Count - 1].Top-40;
+                    this.Snake.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
+                    Controls.Add(Snake);
+                    SnakeLeght.Add(Snake);
                     Meal.Hide();
+                    Meals();
+                }
+                for (int i = 0; i < SnakeLeght.Count-1; i++)
+                {
+                    if (SnakeLeght[SnakeLeght.Count - 1].Top == SnakeLeght[i].Top && SnakeLeght[SnakeLeght.Count - 1].Left == SnakeLeght[i].Left)
+                    {
+                        MessageBox.Show("Lose");
+                        this.Hide();
+                    }
                 }
             }
             if (e.KeyCode == Keys.Down)
@@ -125,10 +196,36 @@ namespace SnakeApp
                         item.Top += 40;
                     }
                 }
+                if (SnakeLeght[SnakeLeght.Count - 1].Top > this.Height)
+                {
+                    int AB;
+                    int AS = this.Height % 40;
+                    AB = this.Height - AS;
+                    SnakeLeght[SnakeLeght.Count - 1].Top = 40;
+                }
+
                 if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
                 {
-                    Snake.BackColor = Color.Red;
+                    Snake = new Button();
+                    Snake.Width = 40;
+                    Snake.Height = 40;
+                    Snake.BackColor = Color.Green;
+                    Snake.Left = SnakeLeght[SnakeLeght.Count - 1].Left;
+                    Snake.FlatStyle = FlatStyle.Flat;
+                    Snake.Top = SnakeLeght[SnakeLeght.Count - 1].Top+40;
+                    this.Snake.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
+                    Controls.Add(Snake);
+                    SnakeLeght.Add(Snake);
                     Meal.Hide();
+                    Meals();
+                }
+                for (int i = 0; i < SnakeLeght.Count-1; i++)
+                {
+                    if (SnakeLeght[SnakeLeght.Count - 1].Top == SnakeLeght[i].Top && SnakeLeght[SnakeLeght.Count - 1].Left == SnakeLeght[i].Left)
+                    {
+                        MessageBox.Show("Lose");
+              
+                    }
                 }
 
             }
@@ -136,16 +233,29 @@ namespace SnakeApp
         void Meals()
         {
             Random rnd = new Random();
-            int MealLeft = rnd.Next(0, this.Width);
-            int MealTop = rnd.Next(0,this.Height);
-            Meal = new Button();
-            Meal.Width = 40;
-            Meal.Height = 30;
-            Meal.BackColor = Color.Red;
-            Meal.Left = MealLeft;
-            Meal.Top = MealTop;
-            Meal.FlatStyle = FlatStyle.Flat;
-            Controls.Add(Meal);
+            int MealLeft = rnd.Next(0, this.Width/40);
+            int MealTop = rnd.Next(0,this.Height/40);
+            MealTop = MealTop * 40;
+            MealLeft = MealLeft * 40;
+            if (MealLeft % 40 == 0 && MealTop % 40 == 0)
+            {
+                Meal = new Button();
+                Meal.Width = 40;
+                Meal.Height = 40;
+                Meal.BackColor = Color.Red;
+                Meal.Left = MealLeft;
+                Meal.Top = MealTop;
+                Meal.FlatStyle = FlatStyle.Flat;
+                Controls.Add(Meal);
+            }
+            else
+            {
+                Meals();
+            }
+            
+        }
+        void AddMeal()
+        {
         }
     }
 }
