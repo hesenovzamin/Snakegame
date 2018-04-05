ef83c76f75b01708a2ef0c8921386ef3640fd311
hesenovzamin <hesenovzamin@gmail.com>
2018-04-05 12:03:05 +0400

    Fimal2

--- SnakeApp/SnakeApp/Form1.Designer.cs
+++ SnakeApp/SnakeApp/Form1.Designer.cs
@@ -30,23 +30,35 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 353);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.ClientSize = new System.Drawing.Size(674, 358);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SnakeApp/SnakeApp/Form1.cs
+++ SnakeApp/SnakeApp/Form1.cs
@@ -14,10 +14,12 @@ namespace SnakeApp
     public partial class Form1 : Form
     {
         public SoundPlayer players = new SoundPlayer();
-       public Timer TimerRight = new Timer();
+        public Timer TimerOyun = new Timer();
+        public Timer TimerRight = new Timer();
         public Timer TimerLeft = new Timer();
         public Timer TimerUp = new Timer();
         public Timer TimerDown = new Timer();
+        public int say = 0;
 
         public List<Button> SnakeLeght = new List<Button>();
         public Button Snake;
@@ -28,13 +30,15 @@ namespace SnakeApp
         {
             InitializeComponent();
             players.SoundLocation = "Snake.wav";
-            TimerRight.Interval = 300;
+            TimerRight.Interval = 200;
+            TimerOyun.Interval = 600;
+            TimerOyun.Tick += new System.EventHandler(this.TimerOyun_Tick);
             TimerRight.Tick += new System.EventHandler(this.TimerRight_Tick);
-            TimerLeft.Interval = 300;
+            TimerLeft.Interval = 200;
             TimerLeft.Tick += new System.EventHandler(this.TimerLeft_Tick);
-            TimerUp.Interval = 300;
+            TimerUp.Interval = 200;
             TimerUp.Tick += new System.EventHandler(this.TimerUp_Tick);
-            TimerDown.Interval = 300;
+            TimerDown.Interval = 200;
             TimerDown.Tick += new System.EventHandler(this.TimerDown_Tick);
 
             Meals();
@@ -51,22 +55,23 @@ namespace SnakeApp
                 this.Snake.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
                 Controls.Add(Snake);
                 SnakeLeght.Add(Snake);
-               
+
 
             }
-            
+
 
         }
-        private void Snake_KeyUp(object sender,KeyEventArgs e)
+        private void Snake_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode==Keys.Right)
+            TimerOyun.Start();
+            if (e.KeyCode == Keys.Right)
             {
                 TimerLeft.Stop();
                 TimerUp.Stop();
                 TimerDown.Stop();
                 TimerRight.Start();
-               
-                    
+
+
 
             }
             if (e.KeyCode == Keys.Left)
@@ -94,13 +99,17 @@ namespace SnakeApp
         }
         void Meals()
         {
+            say++;
+            if (say == 11)
+            {
+                MessageBox.Show("Aferin");
+                this.Hide();
+            }
             Random rnd = new Random();
-            int MealLeft = rnd.Next(0, this.Width/40);
-            int MealTop = rnd.Next(0,this.Height/40);
+            int MealLeft = rnd.Next(0, this.Width / 40);
+            int MealTop = rnd.Next(0, this.Height / 40);
             MealTop = MealTop * 40;
             MealLeft = MealLeft * 40;
-            if (MealLeft % 40 == 0 && MealTop % 40 == 0)
-            {
                 Meal = new Button();
                 Meal.Width = 40;
                 Meal.Height = 40;
@@ -109,12 +118,8 @@ namespace SnakeApp
                 Meal.Top = MealTop;
                 Meal.FlatStyle = FlatStyle.Flat;
                 Controls.Add(Meal);
-            }
-            else
-            {
-                Meals();
-            }
-            
+           
+
         }
 
         private void TimerRight_Tick(object sender, EventArgs e)
@@ -163,7 +168,7 @@ namespace SnakeApp
                     MessageBox.Show("Lose");
                     this.Hide();
                     TimerRight.Stop();
-             
+
                 }
             }
         }
@@ -188,7 +193,7 @@ namespace SnakeApp
                 int AB;
                 int AS = this.Width % 40;
                 AB = this.Width - AS;
-                SnakeLeght[SnakeLeght.Count - 1].Left = AB-40;
+                SnakeLeght[SnakeLeght.Count - 1].Left = AB - 40;
             }
             if ((Snake.Top == Meal.Top) && (Snake.Left == Meal.Left))
             {
@@ -211,7 +216,7 @@ namespace SnakeApp
                 if (SnakeLeght[SnakeLeght.Count - 1].Top == SnakeLeght[i].Top && SnakeLeght[SnakeLeght.Count - 1].Left == SnakeLeght[i].Left)
                 {
                     MessageBox.Show("Lose");
-             
+
                     this.Hide();
                     TimerLeft.Stop();
                 }
@@ -262,7 +267,7 @@ namespace SnakeApp
                 {
                     MessageBox.Show("Lose");
                     this.Hide();
-        
+
                     TimerUp.Stop();
                 }
             }
@@ -283,7 +288,7 @@ namespace SnakeApp
                     item.Top += 40;
                 }
             }
-            if (SnakeLeght[SnakeLeght.Count - 1].Top >this.Height)
+            if (SnakeLeght[SnakeLeght.Count - 1].Top > this.Height)
             {
                 int AB;
                 int AS = this.Height % 40;
@@ -316,6 +321,30 @@ namespace SnakeApp
 
                 }
             }
+        }
+        public static int a = 0;
+        public static int Deq = 0;
+        private void TimerOyun_Tick(object sender, EventArgs e)
+        {
+
+            a++;
+            if (a == 60)
+            {
+                Deq++;
+                a = 0;
+            }
+            if (Deq==3)
+            {
+                TimerLeft.Stop();
+                TimerUp.Stop();
+                TimerDown.Stop();
+                TimerRight.Stop();
+                TimerOyun.Stop();
+                MessageBox.Show("Oyun bitdi qaqa");
+               
+                this.Hide();
+            }
+            label1.Text = "0" + Deq.ToString() + ":" + a.ToString();
         }
     }
 }
